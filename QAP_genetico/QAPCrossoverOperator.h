930d9a6060e58c07dd3ac5cc3885146c3710dc1a
jesuszed <jesus.zed@gmail.com>
2017-05-24 12:15:02 +0200

    Añadido genetico

---
+++ QAP_genetico/QAPCrossoverOperator.h
@@ -0,0 +1,111 @@
+/*
+ * QAPCrossoverOperator.h
+ *
+ * Fichero que define la clase QAPCrossoverOperator. Forma parte del código esqueleto para el problema de las múltiples mochilas cuadráticas, ofrecido para las prácticas de la asignatura Metaheurísticas del Grado de Ingeniería Informática de la Universidad de Córdoba
+ *
+ * @author Carlos García cgarcia@uco.es
+ */
+
+#ifndef INCLUDE_QAPCROSSOVEROPERATOR_H_
+#define INCLUDE_QAPCROSSOVEROPERATOR_H_
+
+#include <vector>
+ #include <cstdlib>
+ 
+#include "QAPSolution.h"
+#include "QAPInstance.h"
+#include "QAPGeneticAlgorithm.h"
+
+using namespace std;
+
+/**
+ * Clase que implementa un operador de cruce uniforme para el problema QAP. Su idea es la misma que la de un cruce uniforme para codificación entera
+ */
+class QAPCrossoverOperator {
+
+protected:
+	/**
+	 * Variables miembro de la clase:
+	 *  - _instance Instancia de problema abordada. Se utiliza únicamente para crear nuevos objetos QAPSolution
+	 *  - _numLocalizaciones almacena el número de objetos de la instancia abordada para reducir el número de consultas a la instancia
+	 *  - _crossProb probabilidad de cruce
+	 */
+	QAPInstance *_instance;
+	unsigned _numLocalizaciones;
+	double _crossProb;
+
+	/**
+	 * Función que cruza dos soluciones según su probabilidad de cruce. En caso de que no haya crucce, la solución devuelta será igual al primer padre
+	 * @param[in] s1 primer padre
+	 * @param[in] s2 segundo padre
+	 * @return Nuevo objeto solución descendiente de haber cruzado s1 y s2. La solución se reserva dinámicamente en memoria. Es responsabilidad del invocador de gestionarla correctamente.
+	 */
+	QAPSolution * cross(Solution *s1, Solution *s2) {
+		QAPSolution * hijo = new QAPSolution(*_instance);
+		QAPSolution * sol1 = (QAPSolution *) s1;
+		QAPSolution * sol2 = (QAPSolution *) s2;
+
+		int aux;
+		int iterador=0;
+
+		double randSample = (((double) rand()) / RAND_MAX);
+
+		if (randSample < _crossProb) {
+
+			//Rellenamos un 50% del hijo, a partir de 2 particiones del 25% del padre1, una al principio y otra al final esto solo lo leemos nosotros
+			for(int i=0;i<_numLocalizaciones/4;i++){
+				hijo->putInstalacion(sol1->whatIsInLocalizacion(i),i);
+			}
+			for(int i=_numLocalizaciones-1; i > (_numLocalizaciones- 1 - _numLocalizaciones/4);i--){
+				hijo->putInstalacion(sol1->whatIsInLocalizacion(i),i);
+			}
+
+			for(int i=0;i<_numLocalizaciones;i++){
+				aux=sol2->whatIsInLocalizacion(i);
+
+				if(!hijo->existeIns(aux)){
+					hijo->putInstalacion(sol1->whatIsInLocalizacion(i),iterador+_numLocalizaciones/4);
+					iterador+=1;
+				}
+
+			}
+
+		} else {
+			//Si no hay cruce, copiar el primer padre
+			hijo->copy(*sol1);
+		}
+
+		return hijo;
+	}
+
+public:
+
+	/**
+	 * Constructor
+	 * @param[in] crossProb Probabilidad de cruce
+	 * @param[in] instance Instancia del problema abordada
+	 */
+	QAPCrossoverOperator(double crossProb, QAPInstance &instance) {
+		_instance = &instance;
+		_numLocalizaciones = instance.getNumLoc();
+		_crossProb = crossProb;
+	}
+
+	/**
+	 * Función que aplica el cruce a una población de padres.
+	 * @param[in] parents Vector de padres. El cruce se aplica entre cada dos padres consecutivos (1,2), (3,4)...
+	 * @param[out] offspring Vector donde se almacenan los descendientes generados. IMPORTANTE: Esta función reserva memoria dinámicamente para las nuevas soluciones en offspring, por lo que es responsabilidad de quien la invoque de gestionar la memoria adecuadamente.
+	 */
+	void cross(vector<Solution*> &parents, vector<Solution*> &offspring) {
+
+		unsigned numParents = (unsigned) parents.size();
+
+		//TODO aplicar cruce entre cada dos padres consecutivos (1,2), (3,4), ...
+		for (int i=0;i<numParents;i+=2) {
+			QAPSolution *sol = cross(parents[i],parents[i+1]);
+			offspring.push_back(sol);
+		}
+	}
+};
+
+#endif /* INCLUDE_QAPCROSSOVEROPERATOR_H_ */
---
+++ QAP_genetico/QAPMutationOperator.h
@@ -0,0 +1,78 @@
+/*
+ * QAPMutationOperator.h
+ *
+ * Fichero que define la clase QAPMutationOperator. Forma parte del código esqueleto para el problema de las múltiples mochilas cuadráticas, ofrecido para las prácticas de la asignatura Metaheurísticas del Grado de Ingeniería Informática de la Universidad de Córdoba
+ *
+ * @author Carlos García cgarcia@uco.es
+ */
+
+#ifndef INCLUDE_QAPMUTATIONOPERATOR_H_
+#define INCLUDE_QAPMUTATIONOPERATOR_H_
+
+#include "QAPSolution.h"
+#include "Solution.h"
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * Clase que define un operador de mutación para el QAP. Se basa en recorrer todos los genes de la solución y cambiarlos por un valor aleatorio según una probabilidad
+ */
+class QAPMutationOperator{
+
+protected:
+	/**
+	 * Variables miembro de la clase
+	 * _mutProb Probabilidad de mutación
+	 * _numLocalizaciones Número de objetos. Se utiliza para reducir el número de consultas a la instancia
+	 * _numInstalaciones Número de mochilas del problema. Se utiliza para reducir el número de consultas a la instancia
+	 */
+	double _mutProb;
+	unsigned _numLocalizaciones;
+	unsigned _numInstalaciones;
+
+	/**
+	 * Función que muta una solución
+	 * @param[in,out] sol Solución a mutar
+	 */
+	void mutate(Solution* sol){
+		QAPSolution *s = (QAPSolution*) sol;
+		int pepito=0;
+		//TODO Recorrer los objetos y, según la probabilidad de mutación,
+		//asignarlos a una mochila aleatoria (podrían modificarse 0, 1, o más de 1 gen)
+		for(int i=0;i<_numLocalizaciones/2;i++){
+			if(_mutProb>(double)(rand()) / RAND_MAX){//REVISAR probabilidad
+				pepito=rand()%(_numInstalaciones);
+				s->intercambio(i,pepito);
+			}
+		}
+	}
+
+public:
+	/**
+	 * Constructor
+	 * @param[in] mutProb Probabilidad de mutación
+	 * @param[in] instance Instancia del problema a abordar
+	 */
+	QAPMutationOperator(double mutProb, QAPInstance &instance){
+		_mutProb = mutProb;
+		_numLocalizaciones = instance.getNumLoc();
+		_numInstalaciones = instance.getNumIns();
+	}
+
+	/**
+	 * Función que muta un conjunto de soluciones
+	 * @param[in,out] sols Soluciones a mutar
+	 */
+	void mutate(vector<Solution*> &sols){
+
+		for (Solution* sol : sols){
+			mutate(sol);
+		}
+	}
+};
+
+
+
+#endif /* INCLUDE_QAPMUTATIONOPERATOR_H_ */
