c0b19f8e85afb059be5f807aa0645eb8cb973bf1
jesuszed <jesus.zed@gmail.com>
2017-05-25 11:12:02 +0200

    Modificado gnuplot aleatorio

--- QAP aleatorio/Grafica/gnuplot.sh
+++ QAP aleatorio/Grafica/gnuplot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cat << _end_ | gnuplot
-set terminal postscript eps color
-set output "grafico.eps"
+set terminal pngcairo size 600,500 enhanced font 'Verdana,10'
+set output 'Aleaotorio.png'
+set title "Aleatorio"
 set key right bottom
-set xlabel "Iteración"
+set xlabel "Iteracion"
 set ylabel "Coste"
 plot 'ejemplo.txt' using 1 t "Mejor Encontrado" w l, 'ejemplo.txt' using 2 t "Valores medios" w l,
 _end_
